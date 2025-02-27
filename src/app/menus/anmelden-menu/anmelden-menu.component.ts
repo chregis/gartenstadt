14bca80f5f533c9de67ea3f0c93acaa443d19fd6
Christian Spiller <christian.spiller@chaosnet.ch>
2025-02-27 14:44:04 +0100

    fix scrollTo

--- src/app/menus/anmelden-menu/anmelden-menu.component.ts
+++ src/app/menus/anmelden-menu/anmelden-menu.component.ts
@@ -1,41 +1,9 @@
-import {AfterViewChecked, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-anmelden-menu',
   templateUrl: './anmelden-menu.component.html',
   styleUrls: ['./anmelden-menu.component.css']
 })
-export class AnmeldenMenuComponent implements OnInit, AfterViewChecked {
-
-  fragment: any;
-
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.fragment.subscribe( (fragment: any) => { this.fragment = fragment; });
-  }
-
-  ngAfterViewChecked(): void {
-    try {
-      if(this.fragment) {
-        // document?.getElementById(this.fragment)?.scrollIntoView();
-        const element = document?.getElementById(this.fragment);
-        if(!element) {
-          return;
-        }
-
-        var headerOffset = 100;
-        var elementPosition = element.getBoundingClientRect().top;
-        var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-        console.log(elementPosition + ' + ' + window.pageYOffset + " - " + headerOffset + " = " + offsetPosition);
-
-        window.scrollTo({
-          top: offsetPosition,
-          behavior: "smooth"
-        });
-      }
-    } catch (e) { }
-  }
-
+export class AnmeldenMenuComponent {
 }
--- src/app/pages/anmeldung/anmeldung.component.ts
+++ src/app/pages/anmeldung/anmeldung.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-anmeldung',
   templateUrl: './anmeldung.component.html',
   styleUrls: ['./anmeldung.component.css']
 })
-export class AnmeldungComponent {
+export class AnmeldungComponent implements OnInit {
 
-  constructor() { }
+  fragment: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.fragment.subscribe( (fragment: any) => {
+      console.log(`fragment ${fragment}`);
+      this.fragment = fragment;
+      this.scroll();
+    });
+  }
+
+  scroll(): void {
+    try {
+      if(this.fragment) {
+        console.log('SCROLL');
+        // document?.getElementById(this.fragment)?.scrollIntoView();
+        const element = document?.getElementById(this.fragment);
+        if(!element) {
+          return;
+        }
+
+        const scrollContainer = document.querySelector(".mat-drawer-content");
+        const scrollPositionY: number = element.offsetTop;
+
+        console.log("Scroll To", scrollPositionY);
+        if(scrollPositionY != null && scrollContainer) {
+          scrollContainer.scrollTo({ top: scrollPositionY + -100, behavior: "smooth" });
+        } else {
+          element.scrollIntoView({behavior: "smooth", block: "center"});
+        }
+
+      }
+    } catch (e) { }
+  }
+
 
 }
